9aab96e93e3b99bf35879d559d31dece2679ef56
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2016-02-09 15:30:57 +0100

    check for #skolesjakken hash

--- app/theme.function.js
+++ app/theme.function.js
@@ -1,13 +1,16 @@
 System.register([], function(exports_1) {
     function setTheme() {
         var themeUrl;
-        if (window.location.hostname == 'skolesjakken.dc-chess.com') {
+        if (window.location.hostname == 'skolesjakken.dc-chess.com' ||
+            (window.location.hash.indexOf('#skolesjakken')) > -1) {
             themeUrl = '../styles/skolesjakken.css';
             console.warn(window.location.hostname);
+            console.warn(window.location.hash);
             console.warn('Doing whitelabel...');
         }
         else {
             themeUrl = '../styles/concise.css';
+            console.log(window.location.hash);
             console.log(window.location.hostname);
             console.log('Not doing whitelabel');
         }
--- app/theme.function.ts
+++ app/theme.function.ts
@@ -1,15 +1,17 @@
 export function setTheme() {
     let themeUrl;
 
-    if (window.location.hostname == 'skolesjakken.dc-chess.com') {
+    if ((window.location.hash.indexOf('#skolesjakken')) > -1) {
         themeUrl = '../styles/skolesjakken.css';
 
         console.warn(window.location.hostname);
+        console.warn(window.location.hash);
         console.warn('Doing whitelabel...');
     }
     else {
         themeUrl = '../styles/concise.css';
 
+        console.log(window.location.hash);
         console.log(window.location.hostname);
         console.log('Not doing whitelabel');
     }
